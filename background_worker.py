69a2f774a05494372f634078fdd4ac7daaf07f9a
Ali Nisar Haider <alinisarhaider@gmail.com>
2020-07-15 15:17:04 +0500

    minor changes

--- background_worker.py
+++ background_worker.py
@@ -6,12 +6,15 @@ from design_html import create_output_html, create_error_html
 
 
 def keyword_detection_processing(url, keywords):
+    print('Retrieving audio')
     audio_segment_buffer = get_audio_stream(video_url=url)
     if type(audio_segment_buffer) == str:
         create_error_html()
         return
+    print('Retrieving transcriptions')
     response, time_offset = transcribe_audio(audio_data=audio_segment_buffer, language='en-US')
     transcription, timestamps_list = get_transcriptions(response_list=response, time_offset_list=time_offset)
+    print('Formalising results')
     detections = get_detections(keywords=keywords, transcription=transcription, timestamps_list=timestamps_list)
     create_output_html(detections=detections)
     return
