289b82bcd5493896caa252fea3fc72f1501353e4
Ali Nisar Haider <alinisarhaider@gmail.com>
2020-07-15 19:02:43 +0500

    final deploy of initial phase

--- background_worker.py
+++ background_worker.py
@@ -2,6 +2,7 @@ from youtube_operations import get_audio_stream
 from speech_to_text_api_operations import transcribe_audio
 from transcription_operations import get_transcriptions
 from detect_keywords import get_detections
+from design_html import create_results_html
 
 
 def keyword_detection_processing(url, keywords):
@@ -11,4 +12,5 @@ def keyword_detection_processing(url, keywords):
     response, time_offset = transcribe_audio(audio_data=audio_segment_buffer, language='en-US')
     transcription, timestamps_list = get_transcriptions(response_list=response, time_offset_list=time_offset)
     detections = get_detections(keywords=keywords, transcription=transcription, timestamps_list=timestamps_list)
+    create_results_html(detections)
     return detections
