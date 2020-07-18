848c969271fd6960bc9dbd389fd54a78eae7652e
Ali Nisar Haider <alinisarhaider@gmail.com>
2020-07-18 18:42:44 +0500

    Limited video length

--- youtube_operations.py
+++ youtube_operations.py
@@ -46,7 +46,7 @@ def get_audio_stream(video_url: str):
     try:
         youtube = MyYouTube(video_url)
     except:
-        return 'No YouTube video found for the given URL. Please try some other video.'
+        return 'Access to this video is restricted by YouTube. Kindly try some other video.'
 
     video_length = int(youtube.player_config_args['player_response']['videoDetails']['lengthSeconds'])
     if video_length > 1260:
